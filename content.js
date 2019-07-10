89d2dcce2eb414566a6a9bf1662f4b177c5e2018
Ryan Voor <rvoor@neadwerx.com>
2019-07-10 15:55:12 -0400

    added background coloring changes and text coloring changes to work better with dark reader

--- content.js
+++ content.js
@@ -7,6 +7,28 @@ var checkPageLoadedYet = setInterval(
                 console.log( 'Putting extra space under the footer (place where you type messages) for ease of viewing on vertical monitors' );
                 document.getElementsByTagName( 'footer' )[0].style.marginBottom = '500px';
             }
+
+            // change colors so it works better with dark reader
+
+            // set sidebar background color to inverse of what it would show up as without dark reader so that with dark reader it will actually stay dark
+            document.getElementsByClassName( 'p-classic_nav__team_header' )[0].style.backgroundColor = '#ffedf9';
+            document.getElementsByClassName( 'c-scrollbar'                )[0].style.backgroundColor = '#ffedf9';
+
+            // change the color of the text inside the sidebar so that it stays visible. We make it black so that dark reader will make it white
+            document.getElementsByClassName( 'p-classic_nav__team_header' )[0].style.color = 'black';
+            document.getElementsByClassName( 'c-scrollbar'                )[0].style.color = 'black';
+
+            // change the text color of the "all unreads" and "threads" links
+            document.getElementsByClassName( 'p-channel_sidebar__link' )[0].style.color = 'black';
+            document.getElementsByClassName( 'p-channel_sidebar__link' )[1].style.color = 'black';
+
+            // change the text coloring for all the channel links and all the direct message links
+            var i;
+            for( i = 0; i < document.getElementsByClassName('p-channel_sidebar__channel').length; i++ )
+            {
+                document.getElementsByClassName('p-channel_sidebar__channel')[i].style.color = 'black';
+            }
+
             clearInterval( checkPageLoadedYet );
         }
     },
