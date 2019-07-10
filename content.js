77eab484ad4c52896ffb341484df2e3a36e7b6ee
Ryan Voor <rvoor@neadwerx.com>
2019-07-10 15:10:59 -0400

    updated extension cuz slack updated their site. URL changed. page took longer to load. and they removed the 'footer' id so I had to identify the element a different way

--- content.js
+++ content.js
@@ -1,8 +1,14 @@
-// when a slack channel is open
-if ( typeof document.getElementById( 'footer' ) !== 'undefined' ) {
-    // make sure we only do this when the slack window is really tall (like when I'm using vertical monitors)
-    if ( document.getElementsByTagName( 'body' )[0].clientHeight > 1200 ) {
-        console.log( 'Putting extra space under the footer (place where you type messages) for ease of viewing on vertical monitors' );
-        document.getElementById( 'footer' ).style.marginBottom = '500px';
-    }
-}
+var checkPageLoadedYet = setInterval(
+    function() {
+        // when a slack channel is open
+        if ( typeof document.getElementsByTagName( 'footer' )[0] !== 'undefined' ) {
+            // make sure we only do this when the slack window is really tall (like when I'm using vertical monitors)
+            if ( document.getElementsByTagName( 'body' )[0].clientHeight > 1200 ) {
+                console.log( 'Putting extra space under the footer (place where you type messages) for ease of viewing on vertical monitors' );
+                document.getElementsByTagName( 'footer' )[0].style.marginBottom = '500px';
+            }
+            clearInterval( checkPageLoadedYet );
+        }
+    },
+    100 // check every 100 ms to see if the page is loaded yet
+);
