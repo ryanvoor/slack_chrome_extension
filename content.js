7bc9b784083cee1523dcc2b976452f3957040d54
Ryan Voor <rvoor@neadwerx.com>
2019-09-12 15:04:50 -0400

    commented out color changing code because slack added a dark theme

--- content.js
+++ content.js
@@ -8,26 +8,27 @@ var checkPageLoadedYet = setInterval(
                 document.getElementsByTagName( 'footer' )[0].style.marginBottom = '500px';
             }
 
-            // change colors so it works better with dark reader
+            // This not defunct because slack updated to include a dark theme
+            //// change colors so it works better with dark reader
 
-            // set sidebar background color to inverse of what it would show up as without dark reader so that with dark reader it will actually stay dark
-            document.getElementsByClassName( 'p-classic_nav__team_header' )[0].style.backgroundColor = '#ffedf9';
-            document.getElementsByClassName( 'c-scrollbar'                )[0].style.backgroundColor = '#ffedf9';
+            //// set sidebar background color to inverse of what it would show up as without dark reader so that with dark reader it will actually stay dark
+            //document.getElementsByClassName( 'p-classic_nav__team_header' )[0].style.backgroundColor = '#ffedf9';
+            //document.getElementsByClassName( 'c-scrollbar'                )[0].style.backgroundColor = '#ffedf9';
 
-            // change the color of the text inside the sidebar so that it stays visible. We make it black so that dark reader will make it white
-            document.getElementsByClassName( 'p-classic_nav__team_header' )[0].style.color = 'black';
-            document.getElementsByClassName( 'c-scrollbar'                )[0].style.color = 'black';
+            //// change the color of the text inside the sidebar so that it stays visible. We make it black so that dark reader will make it white
+            //document.getElementsByClassName( 'p-classic_nav__team_header' )[0].style.color = 'black';
+            //document.getElementsByClassName( 'c-scrollbar'                )[0].style.color = 'black';
 
-            // change the text color of the "all unreads" and "threads" links
-            document.getElementsByClassName( 'p-channel_sidebar__link' )[0].style.color = 'black';
-            document.getElementsByClassName( 'p-channel_sidebar__link' )[1].style.color = 'black';
+            //// change the text color of the "all unreads" and "threads" links
+            //document.getElementsByClassName( 'p-channel_sidebar__link' )[0].style.color = 'black';
+            //document.getElementsByClassName( 'p-channel_sidebar__link' )[1].style.color = 'black';
 
-            // change the text coloring for all the channel links and all the direct message links
-            var i;
-            for( i = 0; i < document.getElementsByClassName('p-channel_sidebar__channel').length; i++ )
-            {
-                document.getElementsByClassName('p-channel_sidebar__channel')[i].style.color = 'black';
-            }
+            //// change the text coloring for all the channel links and all the direct message links
+            //var i;
+            //for( i = 0; i < document.getElementsByClassName('p-channel_sidebar__channel').length; i++ )
+            //{
+            //    document.getElementsByClassName('p-channel_sidebar__channel')[i].style.color = 'black';
+            //}
 
             clearInterval( checkPageLoadedYet );
         }
